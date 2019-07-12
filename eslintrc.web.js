4f26052ad7bfcc9df0fa879d6e7dcc1d0940a897
Beto Muniz <contato@betomuniz.com>
2019-07-12 15:55:38 -0300

    Update ESLint for Front-End development. Add Prettier.

--- eslintrc.web.js
+++ eslintrc.web.js
@@ -1,6 +1,9 @@
 {
   "parser": "babel-eslint",
-  "extends": "airbnb",
+  "extends": [
+    "airbnb",
+    "prettier"
+  ],
   "env": {
     "browser": true,
     "node": true,
@@ -8,6 +11,7 @@
     "es6": true
   },
   "plugins": [
+    "prettier",
     "react",
     "jsx-a11y"
   ],
@@ -19,15 +23,20 @@
     }
   },
   "rules": {
+    "prettier/prettier": [
+      "error"
+    ],
+    "arrow-parens": [
+      2,
+      "always"
+    ],
     "arrow-body-style": [
       2,
       "as-needed"
     ],
+    "implicit-arrow-linebreak": 0,
     "class-methods-use-this": 0,
-    "comma-dangle": [
-      2,
-      "always-multiline"
-    ],
+    "comma-dangle": 0,
     "dot-notation": 0,
     "import/imports-first": 0,
     "import/newline-after-import": 0,
@@ -35,28 +44,31 @@
     "import/no-named-as-default": 0,
     "import/no-unresolved": 2,
     "import/prefer-default-export": 0,
-    "indent": [
-      2,
-      2,
-      {
-        "SwitchCase": 1
-      }
-    ],
     "jsx-a11y/aria-props": 2,
+    "jsx-a11y/click-events-have-key-events": 0,
     "jsx-a11y/heading-has-content": 0,
-    "jsx-a11y/label-has-for": 2,
-    "jsx-a11y/mouse-events-have-key-events": 2,
+    "jsx-a11y/label-has-associated-control": 0,
+    "jsx-a11y/label-has-for": 0,
+    "jsx-a11y/mouse-events-have-key-events": 0,
+    "jsx-a11y/no-access-key": 0,
+    "jsx-a11y/no-static-element-interactions": 0,
     "jsx-a11y/role-has-required-aria-props": 2,
     "jsx-a11y/role-supports-aria-props": 2,
     "max-len": 0,
     "newline-per-chained-call": 0,
     "no-console": 0,
+    "no-plusplus": 0,
+    "no-prototype-builtins": 0,
+    "no-underscore-dangle": 0,
     "no-use-before-define": 0,
     "object-curly-newline": 0,
     "prefer-template": 2,
+    "react/button-has-type": 0,
+    "react/destructuring-assignment": 0,
     "react/forbid-prop-types": 0,
     "react/jsx-filename-extension": 0,
     "react/jsx-no-target-blank": 0,
+    "react/jsx-one-expression-per-line": 0,
     "react/no-array-index-key": 0,
     "react/require-extension": 0,
     "react/self-closing-comp": 0,
